d93c0f9fac00cf53c68007679fe20ff6c04689c8
Criticalstone <kevinhoog@gmail.com>
2017-09-19 09:49:25 +0200

    Changes the try catch to only catch the correct error

--- python/cli.py
+++ python/cli.py
@@ -3,18 +3,16 @@ from datetime import datetime
 
 def readId():
     uid = input("Enter a user ID:")
+    
     try:
         (cardID, first_name, last_name, _) = db.selectUserById(uid)
         #TODO: Get last timelog for this user
-
         #TODO: if an open timelog does not exist, start a new timeentry
         db.startTime(str(cardID))
         print "Time started for ", first_name, last_name , "at", datetime.now().strftime('%H:%M:%S')
         #TODO: else stop the timelog found
-
-    except: 
-        print "User not found\n"
-
+    except TypeError as e: 
+        print "No user with that card ID"
 if __name__ == "__main__":
     print "Joker time logger v0.1"
     while(1):
