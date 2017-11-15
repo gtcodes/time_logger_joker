28f62018e82896f12621da26376ccd912b9bc9c4
Mupperry <vectoralarm@gmail.com>
2017-11-15 15:43:36 +0100

    Skeleton for test-file

---
+++ python/test.py
@@ -0,0 +1,67 @@
+import dbConnection as db
+from datetime import datetime
+import time
+import cli
+from array import *
+
+def readIdTest(randomId): #TODO: Insert randomId and read it back
+    result = 1
+    return result
+def loginTest(): #Relevant?
+    result = 1
+    return result
+def editUserTest(randomId, newRandomId): #TODO: Edit randomId and verify the changes
+    result = 1
+    return result
+def registerUserWithCardIDTest():
+    result = 1
+    return result
+def registerUserTest(): 
+    result = 1
+    return result
+def pruneCardIdInputTest():
+    result = 1
+    return result
+def timeLoggingLoopTest():
+    result = 1
+    return result
+def logTimeTest():
+    result = 1
+    return result
+def toDecimalNumberTest():
+    result = 1
+    return result
+def clearScreenTest(): #Relevant?
+    result = 1
+    return result
+
+def cleanDB(randomId, newRandomId): #TODO: Clean up test entries from DB
+    print("DB cleaned\n")
+
+def generateRandomId():
+    randomId = 1
+    return randomId
+
+if __name__ == "__main__":
+    results = array('b')
+    randomId = generateRandomId()
+    newRandomId = generateRandomId()
+
+    results.append(readIdTest(randomId)) 
+    results.append(loginTest())
+    results.append(editUserTest(randomId, newRandomId))
+    results.append(registerUserWithCardIDTest())
+    results.append(registerUserTest())
+    results.append(pruneCardIdInputTest())
+    results.append(timeLoggingLoopTest())
+    results.append(logTimeTest())
+    results.append(toDecimalNumberTest())
+    results.append(clearScreenTest())
+    cleanDB(randomId, newRandomId)
+
+    for i in results:
+        if i:
+            print("OK")
+        else:
+            print("FAIL")
+    
