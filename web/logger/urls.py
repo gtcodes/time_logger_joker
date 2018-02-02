361ff5d3825cba4039ba1fbc5accdd8e694d352a
Criticalstone <kevinhoog@gmail.com>
2018-02-02 10:36:34 +0100

    Changes urls to django 2.0

--- web/logger/urls.py
+++ web/logger/urls.py
@@ -1,19 +1,28 @@
-from django.conf.urls import url, include
+from django.conf.urls import include
+from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    url(r'^user/', include([
-        url(r'^(?P<request_card_id>[0-9]+)/logs$', views.logs, name='user_logs'),
-        url(r'^(?P<request_card_id>[0-9]+)/$', views.user, name='user'),
-        url(r'^', views.users, name='users'),
+    # user paths
+    re_path('user/(?P<request_card_id>[0-9]+)/logs/', views.logs, name='user_logs'),
+    re_path('user/(?P<request_card_id>[0-9]+)/', views.user, name='user'),
+    path('user/', views.users, name='users'),
+    
+    # team paths
+    path('team/', include([
+        re_path('(?P<request_team_name>[0-9A-Za-z\!\?\#\(\)\-\_\+\$\<\>\,\.\'\%\& ]+)/', include([
+            path('edit/', views.edit_team, name='edit_team'),
+            path('update/', views.update_team, name='update_team'),
+            path('', views.team_detail, name='team_detail'),
+        ])),
+        path('add/', views.add_team, name='add_team'),
+        path('',views.teams, name='teams'),
     ])),
-    url(r'^team/', include([
-        url(r'^add$', views.add_team, name='add_team'),
-        url(r'^(?P<request_team_name>[0-9A-Za-z\!\?\#\\\/\(\)\-\_\+\$\<\>\,\.\'\%\& ]+)$', views.team_detail, name='team_detail'),
-        url(r'^', views.teams, name='teams'),
-    ])),
-    url(r'^(?P<day>\d{4}-\d{1,2}-\d{1,2})/$', views.day, name='day'),
-    url(r'^$', views.index, name='index'),
+
+    # logger
+    re_path('(?P<day>\d{4}-\d{1,2}-\d{1,2})', views.day, name='day'),
+    path('', views.index, name='index'),
+
 ]
